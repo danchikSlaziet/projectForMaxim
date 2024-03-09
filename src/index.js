bc2f9354b8f264c2e185bddbb3409a837f8a21e9
Danis Tazhitdinov <tashitdinov1999@mail.ru>
2024-03-09 15:55:17 +0300

    ff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
 import { MoviesProvider } from "./contexts/MoviesContext";
@@ -10,13 +10,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
         <MoviesProvider>
           <App />
         </MoviesProvider>
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
